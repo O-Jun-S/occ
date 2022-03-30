28033e879b993e579b024c92eb09fec6ae27c2ce
O-Jun-S <jun.shima256@gmail.com>
2022-03-31 00:11:47 +0900

    Add test.

---
+++ Makefile
@@ -0,0 +1,6 @@
+test:
+	./test.sh
+
+clean:
+	rm -f *~ tmp*
+
---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+occ="./target/debug/occ"
+
+assert() {
+    expected="$1"
+    input="$2"
+
+    ${occ} "$input" > tmp.s
+    gcc -static -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" == "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input expected, but got $actual"
+        exit 1
+    fi
+}
+
+cargo build
+
+assert 0 0
+assert 42 42
+
+echo The test ended correctly.
+
