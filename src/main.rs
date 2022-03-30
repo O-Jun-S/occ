36b75fac98a782f705bb0416f5e1085d7f97750f
O-Jun-S <jun.shima256@gmail.com>
2022-03-30 22:43:54 +0900

    Made a compiler that compiles an integer.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    if args.len() != 2 {
+        eprint!("Usage: occ <code>");
+        return;
+    }
+
+    print!(".intel_syntax noprefix\n");
+    print!(".global main\n");
+    print!("main:\n");
+    print!("  mov rax, {}\n", args.nth(1).unwrap());
+    print!("  ret\n");
+    return;
 }
+
