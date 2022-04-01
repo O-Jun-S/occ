b5d63e44edb724c81deb5d7f18c94c0716d260af
O-Jun-S <jun.shima256@gmail.com>
2022-04-01 20:28:36 +0900

    Changed function name.

--- src/lang.rs
+++ src/lang.rs
@@ -2,7 +2,7 @@ pub mod ast;
 pub mod parser;
 
 /// Evaluate expressions.
-pub fn eval_expr(s: &str) {
+pub fn gen_expr(s: &str) {
     parser::expr_parser(s)
         .map(|(_, expr)| expr.gen()).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     print!(".intel_syntax noprefix\n");
     print!(".global main\n");
     print!("main:\n");
-    lang::eval_expr(&args.nth(1).unwrap());
+    lang::gen_expr(&args.nth(1).unwrap());
     print!("  pop rax\n");
     print!("  ret\n");
     return;
